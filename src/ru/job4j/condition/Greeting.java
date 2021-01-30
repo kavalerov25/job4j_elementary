4da13f0cf9a995f5638af671f8647c45d43a7916
kavalerov25 <paradise_777@mail.ru>
2021-01-30 20:00:46 +0300

    класс Greeting

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+    String idea = "I like Java!";
+        System.out.println(idea);
+    String newbie =" But I am a newbie.";
+        System.out.println(idea + newbie);
+    int year = 2021;
+    idea = idea + " " + year;
+        System.out.println(idea);
+    }
+}
