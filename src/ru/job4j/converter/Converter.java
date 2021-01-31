bf9ea48ad7edadf2c5ea712d84d2d807681ebabb
kavalerov25 <paradise_777@mail.ru>
2021-01-31 22:06:38 +0300

    добавил тестирование в Converter

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -8,14 +8,26 @@ public class Converter {
     }
 
     public static int rubleToDollar(int value) {
-        return value/60; /* формула перевода рублей в доллары. */
+        return value / 60; /* формула перевода рублей в доллары. */
     }
 
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         int dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubles are " + dollar + " dollar" );
+        System.out.println("140 rubles are " + dollar + " dollar");
+
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        int in1 = 140;
+        int expected1 = 2;
+        int out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected1 == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
     }
 }
 
