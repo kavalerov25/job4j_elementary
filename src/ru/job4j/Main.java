42f4bc20129b7c11bbf13fdd923da8139ac47b4c
kavalerov25 <paradise_777@mail.ru>
2021-01-31 20:15:00 +0300

    настройл CheckStyle внёс правки

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
 public class Main {
-    @SuppressWarnings("checkstyle:NoWhitespaceAfter")
-    public static void main(String[] args) {
+       public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
