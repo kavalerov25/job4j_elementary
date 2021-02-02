41b789a4e7a5e1124d3c53ce8d1042738bdfb778
kavalerov25 <paradise_777@mail.ru>
2021-02-02 21:34:37 +0300

    сделал тест-метод для class Fit

---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class FitTest {
+
+    @Test
+    public void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman170Then69() {
+        Assert.assertEquals(69, Fit.womanWeight((short) 170), 0.01);
+    }
+}
