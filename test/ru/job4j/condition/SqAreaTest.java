ab231a5540481e5aedb508845d360a8eff288e7c
kavalerov25 <paradise_777@mail.ru>
2021-02-03 22:10:48 +0300

    сделал тест-метод для class SqArea

---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import  org.junit.Assert;
+import static org.junit.Assert.*;
+
+public class SqAreaTest {
+
+    @Test
+    public void square() {
+        int expected = 2;
+        int p = 6;
+        int k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void square1() {
+        int expected = 1;
+        int p = 4;
+        int k = 1;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
