3e762727bbde476c4fdb668a8663ccb4f2739cd7
kavalerov25 <paradise_777@mail.ru>
2021-02-10 10:37:40 +0300

    4.3.6. Math.abs [#454187] Исправил свою ошибку. создать метод для слона

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -2,11 +2,13 @@ package ru.job4j.condition;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-        int rsl = 0;
-        if (x1 - x2 == y1 - y2) {
-            rsl = Math.abs(x2 - x1);
-            rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+        return (Math.abs(x1 - x2) == Math.abs(y1 - y2)) ? Math.abs(x1 - x2) : 0; // лучший способ решить задание
+
+           /* int rsl = 0;
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+                rsl = Math.abs(x2 - x1);
+            }
+            return rsl; */
+        // длинный способ решить задание
         }
-        return rsl;
     }
-}
