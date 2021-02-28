7ce64dda2e88a6e4bf00c50b94580a0387ebce3f
kavalerov25 <paradise_777@mail.ru>
2021-02-28 20:44:40 +0300

    поправил код в class Turn

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class Turn {
     public static int[] back(int[] array) {
         int temp;
-        for (int i = array.length - 1, j = 0; i >= array.length / 2; i--, j++) {
-            temp = array[j];
-            array[j] = array[i];
-            array[i] = temp;
+        for (int i = 0; i < array.length / 2; i++) {
+            temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
         }
         return array;
     }
